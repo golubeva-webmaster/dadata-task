a859b150c6f5b5c5e1909369d3dc89e866f4168c
Marina Golubeva <golubeva.webmaster@gmail.com>
2022-03-14 15:50:34 +0300

    feat: end

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-// API-ключ
-var token = "1158e831e542a485ce1ff5ed5084e110c5d8874a";
+// // Замените на свой API-ключ
+// var token = "7fd18aaabd7d53ffa4846e4521c1f736c13490eb";
 
-// function join(arr ) { // , separator
+// function join(arr /*, separator */) {
 //   var separator = arguments.length > 1 ? arguments[1] : ", ";
 //   return arr
 //     .filter(function (n) {
@@ -47,13 +47,14 @@ var token = "1158e831e542a485ce1ff5ed5084e110c5d8874a";
 //   token: token,
 //   type: "PARTY",
 //   count: 5,
-//   onSelect: showSuggestion, // Вызывается, когда пользователь выбирает одну из подсказок
+//   /* Вызывается, когда пользователь выбирает одну из подсказок */
+//   onSelect: showSuggestion,
 // });
 
+var token = "5c9efac9bc9d22cac10e7d5cdd6aaf528a4129bc";
 let party = document.getElementById("party");
-//let url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party";
-let url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/findById/party";
-let body = { query: "3446019558" };
+let url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party";
+let body = { query: "" };
 
 var options = {
   method: "POST",
@@ -66,22 +67,79 @@ var options = {
   body: JSON.stringify(body),
 };
 
-const getDataByINN = (text) => {
-  options.body = JSON.stringify({ query: text });
-  // console.log("options: ", options);
-  fetch(url, options)
-    .then((response) => response.text())
-    .then((result) => {
-      console.log(JSON.parse(result));
-      if (result.length > 0) {
-        console.log(JSON.parse(result.suggestions[0]));
-        // console.log(JSON.parse(result.name));
-      }
-    })
-    .catch((error) => console.log("error", error));
+async function getData(inn) {
+  options.body = JSON.stringify({ query: inn });
+  let response = await fetch(url, options);
+  return await response.json();
+}
+
+async function Suggestions(e) {
+  let result = await getData(e.target.value);
+
+  if (result.suggestions.length === 0) {
+    setDate(undefined);
+  } else if (result.suggestions.length === 1) {
+    console.log("result.length === 1");
+    setDate(result.suggestions[0]);
+    deleteSuggestions();
+  } else {
+    createSuggestions(result.suggestions);
+  }
+}
+
+// Для мобильного и десктопа разные события
+if (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(
+    navigator.userAgent
+  )
+) {
+  party.addEventListener("input", Suggestions);
+} else {
+  party.addEventListener("keyup", Suggestions);
+}
+
+// Создание подсказок
+const createSuggestions = (arrSug) => {
+  console.log("массив подсказок ", typeof arrSug, arrSug);
+
+  deleteSuggestions();
+
+  // Создать новый
+  party.setAttribute("list", "suggestions");
+  let datalist = document.createElement("datalist");
+  datalist.setAttribute("id", "suggestions");
+  party.after(datalist);
+
+  // Создание списка подсказок
+  Array.from(arrSug).forEach((element, index) => {
+    const option = document.createElement("option");
+    option.value = element.value;
+    datalist.append(option);
+  });
 };
 
-party.addEventListener("input", function (e) {
-  // console.log("INN: ", e.target.value);
-  getDataByINN(e.target.value);
-});
+// Удалить тег с подсказками, если есть
+const deleteSuggestions = () => {
+  const suggestions = document.getElementById("suggestions");
+  if (suggestions) {
+    suggestions.parentNode.removeChild(suggestions);
+  }
+};
+const setDate = (obj) => {
+  console.log("setData ", obj);
+  if (obj === undefined) {
+    document.getElementById("name_short").value = "";
+    document.getElementById("name_full").value = "";
+    document.getElementById("inn_kpp").value = "";
+    document.getElementById("address").value = "";
+  } else {
+    if (obj.data) {
+      document.getElementById("name_short").value = obj.data.name.short;
+      document.getElementById("name_full").value = obj.data.name.full;
+      document.getElementById(
+        "inn_kpp"
+      ).value = `${obj.data.inn} / ${obj.data.kpp}`;
+      document.getElementById("address").value = obj.data.address.value;
+    }
+  }
+};
